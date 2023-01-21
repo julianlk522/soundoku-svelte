3d933df633d1704449f17548dbd7e2e167eb8f9f
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-01-21 14:10:26 -0500

    cell component

---
+++ components/cell.svelte
@@ -0,0 +1,54 @@
+<script>
+    let selectedCell = false
+    let completed = false
+    let value = 5
+</script>
+
+<button class="cell" on:click={() => selectedCell = !selectedCell} class:selected={selectedCell}>
+    {completed ? value : ""}
+</button>
+
+<style>
+.cell {
+  height: calc(70vw / 9);
+  width: calc(70vw / 9);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #eee;
+  font-weight: 700;
+  border: 2px solid #bbb;
+}
+
+@media (min-width: 640px) {
+  .cell {
+    height: min(calc(60vw / 9), 75px);
+    width: min(calc(60vw / 9), 75px);
+    font-size: 1.5rem;
+  }
+}
+
+@media (min-width: 896px) {
+  .cell {
+    height: min(calc(45vw / 9), 75px);
+    width: min(calc(45vw / 9), 75px);
+  }
+}
+
+.completed {
+    background-color: car(--color-secondary);
+}
+
+.selected {
+    background-color: var(--color-primary);
+}
+
+.selected .empty {
+    background-color: var(--color-primary-soft);
+}
+
+.edgeOfLocalBox {
+    border-right: 4px solid black;
+}
+
+</style>
--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
+    import Cell from "../components/cell.svelte";
 </script>
 
 <main>
+    <Cell />
 </main>
 
 <style>
+    main {
+        display: flex;
+        justify-content: center;
+    }
 </style>
