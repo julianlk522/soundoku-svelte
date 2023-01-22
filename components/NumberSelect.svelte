14daa5e904d363401a9499faaafe254aaa53a188
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-01-21 21:54:24 -0500

    NumberSelect and NumberSelectButton components

---
+++ components/NumberSelect.svelte
@@ -0,0 +1,46 @@
+<script lang='ts'>
+    import NumberSelectButton from "./NumberSelectButton.svelte";
+    let nums = Array.from(Array(9)).map((_, i) => i + 1)
+</script>
+
+<div id="grid">
+    {#each nums as num (num)}
+        <NumberSelectButton value={num} />
+    {/each}
+</div>
+
+<style>
+#grid {
+  width: auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+  align-items: center;
+}
+
+/* .timer {
+  text-align: center;
+  margin-top: 1rem;
+  display: inline-block;
+  width: 100%;
+  color: var(--color-accent);
+  text-shadow: 1px 1px 0px var(--color-primary);
+  font-size: 1.5rem;
+}
+
+.errors {
+  margin-top: 1rem;
+  font-size: 1rem;
+  text-align: center;
+}
+
+@media (min-width: 640px) {
+  .errors,
+  .timer {
+    margin-top: 2rem;
+  }
+  .errors {
+    font-size: 1.5rem;
+  }
+} */
+</style>
---
+++ components/NumberSelectButton.svelte
@@ -0,0 +1,39 @@
+<script lang='ts'>
+    export let value: number
+</script>
+
+<button class="selectionButton">{value}</button>
+
+<style>
+.selectionButton {
+  display: flex;
+  height: 0.5rem;
+  width: 0.5rem;
+  padding: 1.5rem;
+  justify-content: center;
+  align-items: center;
+  background: #eee;
+  border-radius: 0.25rem;
+  box-shadow: 0px 0px 4px rgb(0 0 0 / 25%);
+}
+
+.selectionButton:hover {
+  background: #ccc;
+}
+
+.selectionButton:active {
+  box-shadow: 0px 0px 2px 1px rgb(0 0 0 / 25%);
+}
+
+@media (min-width: 640px) {
+  .selectionButton {
+    padding: 2rem;
+  }
+}
+
+@media (min-width: 1280px) {
+  .selectionButton {
+    padding: 3rem;
+  }
+}
+</style>
