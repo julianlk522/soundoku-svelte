102c3550fabf29281758c403eef04ef546e37c23
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-01-23 21:52:49 -0500

    install prettier and prettier-plugin-svelte, add --color-text and --color-text-light global css variables, set global text color/NumberSelectButton text color/errors text color for increased visibility

--- components/NumberSelect.svelte
+++ components/NumberSelect.svelte
@@ -1,62 +1,60 @@
-<script lang='ts'>
-    import NumberSelectButton from "./NumberSelectButton.svelte";
+<script lang="ts">
+	import NumberSelectButton from './NumberSelectButton.svelte'
 
-    let nums = Array.from(Array(9)).map((_, i) => i + 1)
+	let nums = Array.from(Array(9)).map((_, i) => i + 1)
 
-    export let time = ""
-    export let errors = 0
+	export let time = ''
+	export let errors = 0
 </script>
 
 <div class="flex-column">
-    <div id="selection-grid">
-        {#each nums as num (num)}
-            <NumberSelectButton
-                value={num}
-            />
-        {/each}
-    </div>
-    <h3 class="timer">{time}</h3>
-    <h3 id="errors">Errors: {errors}</h3>
+	<div id="selection-grid">
+		{#each nums as num (num)}
+			<NumberSelectButton value={num} />
+		{/each}
+	</div>
+	<h3 class="timer">{time}</h3>
+	<h3 id="errors">Errors: {errors}</h3>
 </div>
 
-
 <style>
-.flex-column {
-    display: flex;
-    flex-direction: column;
-}
+	.flex-column {
+		display: flex;
+		flex-direction: column;
+	}
 
-#selection-grid {
-  width: auto;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 1rem;
-  align-items: center;
-}
+	#selection-grid {
+		width: auto;
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 1rem;
+		align-items: center;
+	}
 
-.timer {
-  text-align: center;
-  margin-top: 1rem;
-  display: inline-block;
-  width: 100%;
-  color: var(--color-accent);
-  text-shadow: 1px 1px 0px var(--color-primary);
-  font-size: 1.5rem;
-}
+	.timer {
+		text-align: center;
+		margin-top: 1rem;
+		display: inline-block;
+		width: 100%;
+		color: var(--color-accent);
+		text-shadow: 1px 1px 0px var(--color-primary);
+		font-size: 1.5rem;
+	}
 
-#errors {
-  margin-top: 1rem;
-  font-size: 1rem;
-  text-align: center;
-}
+	#errors {
+		color: var(--color-text-light);
+		margin-top: 1rem;
+		font-size: 1rem;
+		text-align: center;
+	}
 
-@media (min-width: 640px) {
-  #errors,
-  .timer {
-    margin-top: 2rem;
-  }
-  #errors {
-    font-size: 1.5rem;
-  }
-}
+	@media (min-width: 640px) {
+		#errors,
+		.timer {
+			margin-top: 2rem;
+		}
+		#errors {
+			font-size: 1.5rem;
+		}
+	}
 </style>
--- components/NumberSelectButton.svelte
+++ components/NumberSelectButton.svelte
@@ -1,56 +1,58 @@
-<script lang='ts'>
-    import {selectedNumberStore, remainingCellsStore} from '../src/stores'
-    import {stopAudio, playAudio} from './audio'
-    export let value: number
+<script lang="ts">
+	import { selectedNumberStore, remainingCellsStore } from '../src/stores'
+	import { stopAudio, playAudio } from './audio'
+	export let value: number
 
-    let cellsRemaining = 81
-    remainingCellsStore.subscribe(remaining => cellsRemaining = remaining)
+	let cellsRemaining = 81
+	remainingCellsStore.subscribe((remaining) => (cellsRemaining = remaining))
 
-    function handleNumberSelect(value: number) {
-      selectedNumberStore.set(value)
+	function handleNumberSelect(value: number) {
+		selectedNumberStore.set(value)
 
-      if (cellsRemaining) {
-        stopAudio()
-        playAudio(value - 1)
-      }
-    }
+		if (cellsRemaining) {
+			stopAudio()
+			playAudio(value - 1)
+		}
+	}
 </script>
 
-<button
-    class="selectionButton"
-    on:click={() => handleNumberSelect(value)}    
->{value}</button>
+<button class="selectionButton" on:click={() => handleNumberSelect(value)}
+	>{value}</button
+>
 
 <style>
-.selectionButton {
-  display: flex;
-  height: 0.5rem;
-  width: 0.5rem;
-  padding: 1.5rem;
-  justify-content: center;
-  align-items: center;
-  background: #eee;
-  border-radius: 0.25rem;
-  box-shadow: 0px 0px 4px rgb(0 0 0 / 25%);
-}
+	.selectionButton {
+		color: var(--color-primary);
+		font-size: 1.5rem;
+		font-weight: 700;
+		display: flex;
+		height: 0.5rem;
+		width: 0.5rem;
+		padding: 1.5rem;
+		justify-content: center;
+		align-items: center;
+		background: #eee;
+		border-radius: 0.25rem;
+		box-shadow: 0px 0px 4px rgb(0 0 0 / 25%);
+	}
 
-.selectionButton:hover {
-  background: #ccc;
-}
+	.selectionButton:hover {
+		background: #ccc;
+	}
 
-.selectionButton:active {
-  box-shadow: 0px 0px 2px 1px rgb(0 0 0 / 25%);
-}
+	.selectionButton:active {
+		box-shadow: 0px 0px 2px 1px rgb(0 0 0 / 25%);
+	}
 
-@media (min-width: 640px) {
-  .selectionButton {
-    padding: 2rem;
-  }
-}
+	@media (min-width: 640px) {
+		.selectionButton {
+			padding: 2rem;
+		}
+	}
 
-@media (min-width: 1280px) {
-  .selectionButton {
-    padding: 3rem;
-  }
-}
+	@media (min-width: 1280px) {
+		.selectionButton {
+			padding: 3rem;
+		}
+	}
 </style>
