9e8b657e8e177e21f886600a3b948c664123bcde
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-01-25 09:39:35 -0500

    add Tutorial component

---
+++ components/Tutorial.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { createEventDispatcher } from 'svelte'
+	const dispatch = createEventDispatcher()
+</script>
+
+<div id="tutorial">
+	<h2>Hey! Thanks for checking out Soundoku.</h2>
+	<h3>
+		This is a brief, optional interactive tutorial to walk you through how
+		to play the game.
+	</h3>
+	<div id="buttons">
+		<button>Continue</button>
+		<button on:click={() => dispatch('skip-tutorial')}>Skip tutorial</button
+		>
+	</div>
+</div>
+
+<style>
+	#tutorial {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	#tutorial > * {
+		color: var(--color-text-light);
+	}
+
+	#buttons {
+		display: flex;
+		justify-content: space-evenly;
+		gap: 2rem;
+		margin-top: 2rem;
+	}
+</style>
--- src/App.svelte
+++ src/App.svelte
@@ -1,124 +1,122 @@
 <script lang="ts">
-    import {onMount, onDestroy} from 'svelte'
-    import Board from '../components/Board.svelte'
-	import NumberSelect from "../components/NumberSelect.svelte"
-    import GameOverPopup from '../components/GameOverPopup.svelte';
+	import { onMount, onDestroy } from 'svelte'
+	import Board from '../components/Board.svelte'
+	import NumberSelect from '../components/NumberSelect.svelte'
+	import GameOverPopup from '../components/GameOverPopup.svelte'
+	import Tutorial from '../components/Tutorial.svelte'
 
-    let time = 0
-    let errors = 0
-    let timer: NodeJS.Timeout
-    let gameOver = false
-    let gameReset = false
+	let tutorial = true
+	let time = 0
+	let errors = 0
+	let timer: NodeJS.Timeout
+	let gameOver = false
+	let gameReset = false
 
-    function formatSeconds(totalSeconds: number) {
-        const minutes = Math.floor(totalSeconds / 60);
-        const remainder = totalSeconds % 60;
-        const seconds = remainder < 10 ? '0' + remainder : remainder;
+	function formatSeconds(totalSeconds: number) {
+		const minutes = Math.floor(totalSeconds / 60)
+		const remainder = totalSeconds % 60
+		const seconds = remainder < 10 ? '0' + remainder : remainder
 
-        return `${minutes}: ${seconds}`;
-    }
+		return `${minutes}: ${seconds}`
+	}
 
-    function handleWin() {
-        clearInterval(timer)
-        gameOver = true
-    }
+	function handleWin() {
+		clearInterval(timer)
+		gameOver = true
+	}
 
-    function handleNewGame() {
-        time = 0
-        timer = setInterval(() => time++, 1000)
-        errors = 0
-        gameOver = false
-        gameReset = true
-        setTimeout(() => gameReset = false, 0)
-    }
+	function handleNewGame() {
+		time = 0
+		timer = setInterval(() => time++, 1000)
+		errors = 0
+		gameOver = false
+		gameReset = true
+		setTimeout(() => (gameReset = false), 0)
+	}
 
-    onMount(() => {
-        clearInterval(timer)
-        time = 0
-        timer = setInterval(() => time++, 1000)
-    })
+	onMount(() => {
+		clearInterval(timer)
+		time = 0
+		timer = setInterval(() => time++, 1000)
+	})
 
-    onDestroy(() => clearInterval(timer))
+	onDestroy(() => clearInterval(timer))
 </script>
 
-{#if !gameReset}
-<main
-    class:game-over-fadeout={gameOver}
->
-    <div class="leavesBg"></div>
-    <Board
-    on:incorrect-guess={() => errors++}
-    on:win={handleWin}
-    />
-    <NumberSelect
-        time={formatSeconds(time)}
-        {errors}
-    />
-</main>
+{#if !gameReset && !tutorial}
+	<main class:game-over-fadeout={gameOver}>
+		<div class="leavesBg" />
+		<Board on:incorrect-guess={() => errors++} on:win={handleWin} />
+		<NumberSelect time={formatSeconds(time)} {errors} />
+	</main>
 {/if}
 
 {#if gameOver}
-    <GameOverPopup
-        victoryTime={formatSeconds(time)}
-        {errors}
-        on:new-game={handleNewGame}
-    />
+	<GameOverPopup
+		victoryTime={formatSeconds(time)}
+		{errors}
+		on:new-game={handleNewGame}
+	/>
+{/if}
+
+{#if tutorial}
+	<Tutorial on:skip-tutorial={() => (tutorial = false)} />
 {/if}
 
 <style>
-    main {
-        position: relative;
-        height: 100%;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background-image: linear-gradient(
-            200deg,
-            hsl(240deg, 100%, 35%, 95%) -5%,
-            hsl(224deg, 95%, 41%, 80%) 0%,
-            hsl(218deg, 90%, 43%, 65%) 8%,
-            hsl(213deg, 85%, 44%, 50%) 24%,
-            hsl(207deg, 80%, 43%, 35%) 45%,
-            hsl(200deg, 75%, 40%, 20%) 62%,
-            hsl(192deg, 70%, 36%, 15%) 75%,
-            hsl(181deg, 65%, 32%, 10%) 85%,
-            hsl(171deg, 60%, 34%, 5%) 93%,
-            hsl(164deg, 55%, 36%, 0%) 100%
-        );
-    }
+	main {
+		position: relative;
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background-image: linear-gradient(
+			200deg,
+			hsl(240deg, 100%, 35%, 95%) -5%,
+			hsl(224deg, 95%, 41%, 80%) 0%,
+			hsl(218deg, 90%, 43%, 65%) 8%,
+			hsl(213deg, 85%, 44%, 50%) 24%,
+			hsl(207deg, 80%, 43%, 35%) 45%,
+			hsl(200deg, 75%, 40%, 20%) 62%,
+			hsl(192deg, 70%, 36%, 15%) 75%,
+			hsl(181deg, 65%, 32%, 10%) 85%,
+			hsl(171deg, 60%, 34%, 5%) 93%,
+			hsl(164deg, 55%, 36%, 0%) 100%
+		);
+	}
 
-    .leavesBg {
-        height: 35%;
-        width: 100%;
-        background-image: url('assets/leavesBg.svg');
-        position: absolute;
-        z-index: -1;
-        opacity: 50%;
-    }
+	.leavesBg {
+		height: 35%;
+		width: 100%;
+		background-image: url('assets/leavesBg.svg');
+		position: absolute;
+		z-index: -1;
+		opacity: 50%;
+	}
 
-    .game-over-fadeout {
-        opacity: 0.1;
-        filter: blur(8px);
-    }
+	.game-over-fadeout {
+		opacity: 0.1;
+		filter: blur(8px);
+	}
 
-    @media (min-width: 640px) {
-        .leavesBg {
-            top: 0;
-        }
-    }
+	@media (min-width: 640px) {
+		.leavesBg {
+			top: 0;
+		}
+	}
 
-    @media (min-width: 896px) {
-        main {
-            flex-direction: row;
-            justify-content: space-evenly;
-        }
+	@media (min-width: 896px) {
+		main {
+			flex-direction: row;
+			justify-content: space-evenly;
+		}
 
-        .leavesBg {
-            height: 20%;
-            top: initial;
-            bottom: 0;
-        }
-    }
+		.leavesBg {
+			height: 20%;
+			top: initial;
+			bottom: 0;
+		}
+	}
 </style>
