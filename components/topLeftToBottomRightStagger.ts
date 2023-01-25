52301c2e331b104682214f910e64005592e928a9
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-01-24 20:28:32 -0500

    edit topLeftToBottomRightStagger func to replace hard-coded calculcations with ones using sideLength argument

--- components/topLeftToBottomRightStagger.ts
+++ components/topLeftToBottomRightStagger.ts
@@ -10,17 +10,20 @@ export function topLeftToBottomRightStagger(index: number, sideLength: number) {
 		return hashMap.get(index)
 	} else if (index < sideLength) {
 		return index + 1 + topLeftToBottomRightStagger(index - 1, sideLength)
-	} else if (Math.floor(index / sideLength) + (index % sideLength) <= 8) {
+	} else if (
+		Math.floor(index / sideLength) + (index % sideLength) <=
+		sideLength - 1
+	) {
 		return (
-			Math.floor(index / 9) +
+			Math.floor(index / sideLength) +
 			(index % sideLength) +
 			topLeftToBottomRightStagger(index - sideLength, sideLength)
 		)
 	} else {
 		return (
 			sideLength -
-			Math.floor(index / 9) +
-			(sideLength - (index % 9)) -
+			Math.floor(index / sideLength) +
+			(sideLength - (index % sideLength)) -
 			1 +
 			topLeftToBottomRightStagger(index - sideLength, sideLength)
 		)
