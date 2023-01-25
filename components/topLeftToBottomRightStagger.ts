8913dd230d24115d93a1848ca6d422d9c3414f5b
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-01-25 09:03:16 -0500

    simplify topLeftToBottomRightStagger formula

--- components/topLeftToBottomRightStagger.ts
+++ components/topLeftToBottomRightStagger.ts
@@ -1,14 +1,9 @@
 export function topLeftToBottomRightStagger(index: number, sideLength: number) {
 	if (index >= Math.pow(sideLength, 2)) return
 
-	const hashMap = new Map([
-		[0, 0],
-		[1, 2],
-	])
-
-	if (index === 0 || index === 1) {
-		return hashMap.get(index)
-	} else if (index < sideLength) {
+	if (index === 0) return 0
+	else if (index === 1) return 2
+	else if (index < sideLength) {
 		return index + 1 + topLeftToBottomRightStagger(index - 1, sideLength)
 	} else if (
 		Math.floor(index / sideLength) + (index % sideLength) <=
