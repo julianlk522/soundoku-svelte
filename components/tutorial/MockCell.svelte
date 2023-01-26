c8292f13dd561182c989ee3f9d1442c9eeb4a192
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-01-26 18:41:39 -0500

    add key block in MockCell to allow replaying transition as cell class changes, add prop defaults, rename classes for clarity

--- components/tutorial/MockCell.svelte
+++ components/tutorial/MockCell.svelte
@@ -1,19 +1,29 @@
 <script lang="ts">
+	import { sineIn } from 'svelte/easing'
+	import { fade } from 'svelte/transition'
+	import { topLeftToBottomRightStagger } from '../utils/topLeftToBottomRightStagger'
 	export let value: number
-	export let active: boolean
-	export let cycles: number
+	export let active: boolean = false
+	export let cycles: number = 0
 	$: cellHueSecondary = active && cycles && Math.floor(cycles / 2) % 2 === 0
 	$: cellHueTertiary = active && cycles && Math.floor(cycles / 2) % 3 === 0
 </script>
 
-<button
-	class="cell"
-	class:active
-	class:cell-hue-secondary={cellHueSecondary}
-	class:cell-hue-tertiary={cellHueTertiary}
->
-	{value}
-</button>
+{#key Math.floor(cycles / 2)}
+	<button
+		class="cell"
+		class:cell-hue-secondary={active}
+		class:cell-hue-tertiary={cellHueSecondary}
+		class:cell-hue-quaternary={cellHueTertiary}
+		in:fade={{
+			duration: 200,
+			easing: sineIn,
+			delay: 50 + 10 * topLeftToBottomRightStagger(value - 1, 3),
+		}}
+	>
+		{value}
+	</button>
+{/key}
 
 <style>
 	.cell {
@@ -25,16 +35,16 @@
 		border: 1px solid #aaa;
 	}
 
-	.active {
+	.cell-hue-secondary {
 		background-color: var(--color-primary-soft);
 		color: var(--color-text-light);
 	}
 
-	.cell-hue-secondary {
+	.cell-hue-tertiary {
 		background-color: var(--color-accent-muted);
 	}
 
-	.cell-hue-tertiary {
+	.cell-hue-quaternary {
 		background-color: var(--color-secondary-soft);
 		color: var(--color-text);
 	}
