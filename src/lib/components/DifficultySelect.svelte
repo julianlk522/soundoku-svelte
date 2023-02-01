d08e937c393af76d5b9c7d0530e0a3135006bcf2
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-02-01 09:28:38 -0500

    add button background colors in DifficultySelect component

--- src/lib/components/DifficultySelect.svelte
+++ src/lib/components/DifficultySelect.svelte
@@ -11,11 +11,45 @@
 </script>
 
 {#each difficulties as difficultyLevel (difficultyLevel)}
-	<button on:click={handleDifficultySelect}>{difficultyLevel}</button>
+	<button
+		id={difficultyLevel.toLowerCase().replace(' ', '-')}
+		on:click={handleDifficultySelect}>{difficultyLevel}</button
+	>
 {/each}
 
 <style>
+	* {
+		--color-difficulty-very-easy: hsl(170deg, 70%, 50%);
+		--color-difficulty-easy: hsl(110deg, 60%, 55%);
+		--color-difficulty-medium: hsl(40deg, 70%, 50%);
+		--color-difficulty-hard: hsl(20deg, 80%, 45%);
+		--color-difficulty-very-hard: hsl(0, 90%, 40%);
+	}
+
 	button {
+		color: var(--color-text);
 		margin: 0 0.5rem;
+		border: none;
+		border-radius: 2px;
+	}
+
+	#very-easy {
+		background-color: var(--color-difficulty-very-easy);
+	}
+
+	#easy {
+		background-color: var(--color-difficulty-easy);
+	}
+
+	#medium {
+		background-color: var(--color-difficulty-medium);
+	}
+
+	#hard {
+		background-color: var(--color-difficulty-hard);
+	}
+
+	#very-hard {
+		background-color: var(--color-difficulty-very-hard);
 	}
 </style>
