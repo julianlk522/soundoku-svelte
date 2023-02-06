6eca0f47cf07afd4212b7e102a0475200b66f9f1
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-02-06 13:27:10 -0500

    add lastToneIndex in audio.ts and set on every call to playAudio() to detect and prevent throttling attackTime when clicking the same cell, add attackTimeRateOfChange for clarity and some explanatory comments

--- src/lib/utils/audio.ts
+++ src/lib/utils/audio.ts
@@ -41,11 +41,13 @@ let audioCtx = new AudioContext()
 let oscillator: OscillatorNode
 let gainNode: GainNode
 let currentTimeAtLastNote = 0
+let lastToneIndex: number | undefined = undefined
 let waitTime = 0
 
-const minimumTimeDeltaBeforeThrottlingAttack = 0.1
+const minimumTimeDeltaBeforeThrottlingAttack = 0.15
 const minAttackTime = 0.01
 const maxAttackTime = 0.2
+const attackTimeRateOfChange = 0.5
 let attackTime = minAttackTime
 const decayTime = 0.1
 const releaseVolume = 0.25
@@ -57,16 +59,18 @@ export function playAudio(
 	panning?: number,
 	intonation?: undefined | 'staccato'
 ) {
+	//	let notes play their full duration by default, only cancel them prematurely if they are played rapidly
 	if (waitTime && waitTime < minimumTimeDeltaBeforeThrottlingAttack) {
 		stopAudio()
 	}
-	init(panning)
+	//	skip getNewAttackTime() if same note being played successively... causes some flickering from rapid/start stop but better than hearing nothing when clicking a cell quickly I think
+	const needNewAttackTime = toneIndex !== lastToneIndex
+	init(panning, needNewAttackTime)
+	oscillator.frequency.value = notes[toneIndex].frequency
 
 	const now = audioCtx.currentTime
 	const duration = intonation === 'staccato' ? noteDuration / 2 : noteDuration
 	const peakVolume = intonation === 'staccato' ? 0.75 : 0.5
-
-	oscillator.frequency.value = notes[toneIndex].frequency
 
 	//	silence tone immediately then fade in to prevent jumbled tones when rapidly playing notes
 	gainNode.gain.exponentialRampToValueAtTime(0.00001, now)
@@ -89,12 +93,16 @@ export function playAudio(
 
 	oscillator.start(now)
 	oscillator.stop(now + duration)
+
+	currentTimeAtLastNote = audioCtx.currentTime
+	lastToneIndex = toneIndex
 }
 
-function init(panning?: number) {
+function init(panning?: number, needNewAttackTime?: boolean) {
 	oscillator = audioCtx.createOscillator()
 	gainNode = audioCtx.createGain()
 
+	//	set panning if needed, otherwise connect oscillator directly to gainNode
 	if (panning) {
 		const panner = new StereoPannerNode(audioCtx, {
 			pan: panning,
@@ -109,37 +117,29 @@ function init(panning?: number) {
 	//	analyze waitTime, set next attackTime accordingly
 	if (currentTimeAtLastNote) {
 		waitTime = audioCtx.currentTime - currentTimeAtLastNote
-		getNewAttackTime()
+		if (needNewAttackTime) getNewAttackTime()
 	}
-
-	currentTimeAtLastNote = audioCtx.currentTime
 }
 
 function stopAudio() {
 	if (!oscillator) return
 
-	if (
-		currentTimeAtLastNote &&
-		audioCtx.currentTime - currentTimeAtLastNote <
-			minimumTimeDeltaBeforeThrottlingAttack
-	) {
-		gainNode.gain.exponentialRampToValueAtTime(
-			0.00001,
-			audioCtx.currentTime + minAttackTime
-		)
-		oscillator.stop(audioCtx.currentTime + minAttackTime)
-	}
+	gainNode.gain.exponentialRampToValueAtTime(
+		0.00001,
+		audioCtx.currentTime + minAttackTime
+	)
+	oscillator.stop(audioCtx.currentTime + minAttackTime)
 }
 
 function getNewAttackTime() {
 	if (waitTime < minimumTimeDeltaBeforeThrottlingAttack) {
 		attackTime = Math.min(
-			attackTime + 0.5 * (maxAttackTime - attackTime),
+			attackTime + attackTimeRateOfChange * (maxAttackTime - attackTime),
 			maxAttackTime
 		)
 	} else {
 		attackTime = Math.max(
-			attackTime - 0.5 * (attackTime - minAttackTime),
+			attackTime - attackTimeRateOfChange * (attackTime - minAttackTime),
 			minAttackTime
 		)
 	}
