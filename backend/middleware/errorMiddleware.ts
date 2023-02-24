1b0a0782dda608bfe1f7b857755d4ed75cf6dc51
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-02-24 13:12:13 -0500

    edit errorHandler to include NextFunction arg to fix broken API endpoint responses

--- backend/middleware/errorMiddleware.ts
+++ backend/middleware/errorMiddleware.ts
@@ -1,9 +1,11 @@
-import { Request, Response, ErrorRequestHandler } from 'express'
+import { Request, Response, ErrorRequestHandler, NextFunction } from 'express'
 
 export const errorHandler: ErrorRequestHandler = (
 	err: Error,
 	_: Request,
-	res: Response
+	res: Response,
+	//	need next arg to prevent "res.status is not a function" errors even while it is not being used
+	next: NextFunction
 ) => {
 	const statusCode = res.statusCode ? res.statusCode : 500
 	res.status(statusCode)
