cc75671f09bd0f552bcf835b34db1f1bc64ae01b
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-02-26 18:17:00 -0500

    protect addWin() endpoint callback with authMiddleware

--- backend/routes/winRoutes.ts
+++ backend/routes/winRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { addWin, getWins } from '../controllers/winsController'
+import protect from '../middleware/authMiddleware'
 
 const router = express.Router()
 
-router.route('/').get(getWins).post(addWin)
+router.route('/').get(getWins).post(protect, addWin)
 
 export default router
