5be3ead3f2a3f12db34e88a8e0690add558fdd0a
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-02-27 13:00:50 -0500

    reorder routes/middleware in server.ts to correctly apply middleware to all routes

--- backend/server.ts
+++ backend/server.ts
@@ -4,19 +4,19 @@ import cors from 'cors'
 import pool from './config/db'
 
 const app = express()
+
 const PORT = process.env.PORT || 5000
 
-// //  connect to MySQL db
+//	connect to MySQL db
 pool.query('SELECT * FROM users LIMIT 1;', (err: any) => {
 	if (err) throw err
 	console.log('Queries are working!')
 })
 
-//  middleware
+//  general middleware
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
-app.use(errorHandler)
 
 //  routes
 import userRoutes from './routes/userRoutes'
@@ -24,5 +24,8 @@ app.use('/users', userRoutes)
 
 import winRoutes from './routes/winRoutes'
 app.use('/wins', winRoutes)
+
+//	error handling
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
