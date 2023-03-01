25190f8d817679d22a1a42e166d608c73d0df1af
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-03-01 14:21:50 -0500

    merge from backend branch: refactor audio.ts to silence cacophony from holding down a number key, add clarifying comments and move some contents of playAudio() func into new generateOscillation() func

--- src/lib/utils/audio.ts
+++ src/lib/utils/audio.ts
@@ -42,9 +42,10 @@ let oscillator: OscillatorNode
 let gainNode: GainNode
 
 let currentTimeAtLastNote = 0
-let waitTime = 0
+let waitTime: number | undefined = undefined
 
-const minimumTimeDeltaBeforeThrottlingAttack = 0.15
+const minimumWaitTimeToThrottleAttackViaNavigation = 0.15
+const minimumWaitTimeToThrottleAttackViaKeys = 0.1
 const minAttackTime = 0.01
 const maxAttackTime = 0.2
 const attackTimeRateOfChange = 0.5
@@ -59,20 +60,62 @@ export function playAudio(
 	toneIndex: number,
 	triggeredByNavigation: boolean = false,
 	panning?: number,
-	intonation?: undefined | 'staccato'
+	intonation?: 'staccato'
 ) {
-	//	let notes play their full duration by default, only cancel them prematurely if triggeredByNavigation AND they are played rapidly
-	if (
-		triggeredByNavigation &&
-		waitTime &&
-		waitTime < minimumTimeDeltaBeforeThrottlingAttack
-	) {
-		stopAudio()
-	}
-	//	get new attack time only if audio triggered by navigation
-	//	prevents cacophony from holding down one of the navigation buttons and triggering new notes at max speed
-	init(panning, triggeredByNavigation)
+	waitTime = audioCtx.currentTime - currentTimeAtLastNote
+	//	let notes play their full duration by default, only cancel them prematurely if they are played rapidly via navigation keys, or if they are played extremely rapidly via number keys
 
+	//	prevents cacophony from holding down one of the navigation buttons and triggering new notes at max speed
+
+	const spammingNotes =
+		waitTime <= minimumWaitTimeToThrottleAttackViaKeys ||
+		(triggeredByNavigation &&
+			waitTime < minimumWaitTimeToThrottleAttackViaNavigation)
+
+	//	the reason for different thresholds is that rapidly navigating over spaces that are partly filled/partly empty will cause waitTime to momentarily spike above the amount achieved by constant activations (due to the extra waitTime from crossing the empty spaces) resulting in noticeable tonal blips
+
+	//	raising minimumWaitTimeToThrottleAttackViaNavigation mostly counteracts this effect, though a board with with 3-4 empty spaces in a row is still prone to blips if those cells are rapidly navigated over
+
+	//	Todo: find a better solution
+
+	if (spammingNotes) {
+		stopAudio()
+		getNewAttackTime()
+	} else if (waitTime > minimumWaitTimeToThrottleAttackViaNavigation) {
+		attackTime = minAttackTime
+	}
+
+	//	abort early if spammingNotes via number keys: no reason to hear same tone more than once
+	//	when calling via navigation keys it may be nice to hear the last note that was triggered
+
+	if (!triggeredByNavigation && spammingNotes) {
+		return (currentTimeAtLastNote = audioCtx.currentTime)
+	}
+
+	init(panning)
+	generateOscillation(toneIndex, intonation)
+
+	currentTimeAtLastNote = audioCtx.currentTime
+}
+
+function init(panning?: number) {
+	oscillator = audioCtx.createOscillator()
+	gainNode = audioCtx.createGain()
+
+	//	set panning if needed, otherwise connect oscillator directly to gainNode
+	if (panning) {
+		const panner = new StereoPannerNode(audioCtx, {
+			pan: panning,
+		})
+		oscillator.connect(panner)
+		panner.connect(gainNode)
+	} else {
+		oscillator.connect(gainNode)
+	}
+	gainNode.connect(audioCtx.destination)
+}
+
+function generateOscillation(toneIndex: number, intonation?: 'staccato') {
 	oscillator.frequency.value = notes[toneIndex].frequency
 
 	const now = audioCtx.currentTime
@@ -100,31 +143,6 @@ export function playAudio(
 
 	oscillator.start(now)
 	oscillator.stop(now + duration)
-
-	currentTimeAtLastNote = audioCtx.currentTime
-}
-
-function init(panning?: number, triggeredByNavigation?: boolean) {
-	oscillator = audioCtx.createOscillator()
-	gainNode = audioCtx.createGain()
-
-	//	set panning if needed, otherwise connect oscillator directly to gainNode
-	if (panning) {
-		const panner = new StereoPannerNode(audioCtx, {
-			pan: panning,
-		})
-		oscillator.connect(panner)
-		panner.connect(gainNode)
-	} else {
-		oscillator.connect(gainNode)
-	}
-	gainNode.connect(audioCtx.destination)
-
-	//	analyze waitTime, set next attackTime accordingly
-	if (currentTimeAtLastNote) {
-		waitTime = audioCtx.currentTime - currentTimeAtLastNote
-		if (triggeredByNavigation) getNewAttackTime()
-	}
 }
 
 function stopAudio() {
@@ -138,7 +156,7 @@ function stopAudio() {
 }
 
 function getNewAttackTime() {
-	if (waitTime < minimumTimeDeltaBeforeThrottlingAttack) {
+	if (waitTime! < minimumWaitTimeToThrottleAttackViaNavigation) {
 		attackTime = Math.min(
 			attackTime + attackTimeRateOfChange * (maxAttackTime - attackTime),
 			maxAttackTime
