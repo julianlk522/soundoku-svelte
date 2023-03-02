f509282e9baff39705cb73b8d493c73284f00fe3
Julian Lindsay-Kaufman <jxl1729@miami.edu>
2023-03-01 20:59:57 -0500

    add scoreIndicator in UserInfo.svelte, set scoreIndicator and briefly display when lastWinScoreStore is updated

--- frontend/src/lib/components/UserInfo.svelte
+++ frontend/src/lib/components/UserInfo.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte'
-	import { loggedInUserStore } from '../../stores'
+	import { fade, fly } from 'svelte/transition'
+	import { lastWinScoreStore, loggedInUserStore } from '../../stores'
 	import { getUserScore } from '../data'
 
 	let name = $loggedInUserStore.name
@@ -9,6 +10,16 @@
 	const token = $loggedInUserStore.token
 		? $loggedInUserStore.token
 		: undefined
+	let scoreIndicator = ''
+	$: $lastWinScoreStore && setScoreIndicator($lastWinScoreStore)
+
+	function setScoreIndicator(score: number) {
+		scoreIndicator = `😎 +${score}`
+		setTimeout(() => {
+			lastWinScoreStore.set(undefined)
+			scoreIndicator = ''
+		}, 2000)
+	}
 
 	onMount(async () => {
 		if (!token) return
@@ -25,19 +36,30 @@
 	})
 </script>
 
-<p>
+<p id="user-info">
 	{name}
 	<span class="placeholder">|</span>
 	{$loggedInUserStore.total_score ?? 0}
 </p>
 
+{#if scoreIndicator}
+	<span transition:fade>
+		<p id="updated-score-indicator" in:fly={{ y: 10 }} out:fly={{ y: -20 }}>
+			{scoreIndicator}
+		</p>
+	</span>
+{/if}
+
 <style>
 	* {
 		color: var(--color-text-light);
 	}
-	p {
+	#user-info,
+	#updated-score-indicator {
 		display: inline-block;
 		position: absolute;
+	}
+	#user-info {
 		bottom: 2rem;
 		right: 2rem;
 		padding: 2px 8px;
@@ -46,5 +68,11 @@
 	}
 	.placeholder {
 		padding: 0 1rem;
+	}
+	#updated-score-indicator {
+		color: var(--color-secondary);
+		font-size: 0.75rem;
+		bottom: 4rem;
+		right: 3rem;
 	}
 </style>
